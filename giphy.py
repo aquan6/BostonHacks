e8c124cb33451e7ec9992c3b17faf6bf31c25331
Dennis Wu <wud.bu.edu>
2017-10-28 20:50:08 -0400

    input is numbers

--- giphy.py
+++ giphy.py
@@ -1,14 +1,24 @@
 import json
 import urllib.request as ur
 
-def get_next_giphy(search_term):
+def get_next_giphy(x, y):
     
+	good_threshold = 40
+	bad_threshold = 20
+
     #search_term = "happy"
     API_KEY = "bv9xw5KEmBxkF8FglWO2dd9iAU0ze9Wr"
     limit = "1"
-    
-    
-    
+    avg = (x+y)/2
+
+    if avg<bad_threshold:
+    	search_term = "bad"
+
+    elif avg>bad_threshold and avg<good_threshold:
+    	search_term = "not+bad"
+
+    else:
+    	search_term = "good"
     
     data = json.loads(ur.urlopen("http://api.giphy.com/v1/gifs/search?q=" + search_term + "&api_key=" + API_KEY + "&limit="+limit).read())
         
