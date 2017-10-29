5bc062db21067379a8f77caeb67edf65f709d2db
Dennis Wu <wud.bu.edu>
2017-10-28 21:07:52 -0400

    Randomized

--- giphy.py
+++ giphy.py
@@ -1,29 +1,30 @@
 import json
 import urllib.request as ur
+import random as random
 
 def get_next_giphy(x, y):
     
-	good_threshold = 40
-	bad_threshold = 20
-
-    #search_term = "happy"
+    good_threshold = 40
+    bad_threshold = 20
+    
     API_KEY = "bv9xw5KEmBxkF8FglWO2dd9iAU0ze9Wr"
     limit = "1"
+    offset = str(random.randrange(25))
     avg = (x+y)/2
-
-    if avg<bad_threshold:
-    	search_term = "bad"
-
-    elif avg>bad_threshold and avg<good_threshold:
-    	search_term = "not+bad"
-
-    else:
-    	search_term = "good"
     
-    data = json.loads(ur.urlopen("http://api.giphy.com/v1/gifs/search?q=" + search_term + "&api_key=" + API_KEY + "&limit="+limit).read())
+    
+    if avg<=bad_threshold:
+        search_term = "bad"
         
-    #print(json.dumps(data, sort_keys=True, indent=4))
+    elif avg>bad_threshold and avg<good_threshold:
+        search_term = "not+bad"
+        
+    else:
+        search_term = "good"
+        
+    data = json.loads(ur.urlopen("http://api.giphy.com/v1/gifs/search?q=" + search_term + "&api_key=" + API_KEY + "&limit="+limit + "&offset=" + offset).read())
+    
     print(data['data'][0]['embed_url'])
     
-    
-#get_next_giphy()
+
+get_next_giphy(50, 50)
