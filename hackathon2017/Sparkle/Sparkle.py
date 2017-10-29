112a1304805e871e4663bebeb4b892074d1b912f
janelljchen <janellc@bu.edu>
2017-10-28 20:37:11 -0400

    Calls
    results.html

--- hackathon2017/Sparkle/Sparkle.py
+++ hackathon2017/Sparkle/Sparkle.py
@@ -5,21 +5,24 @@ import json, urllib
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def display():
     return render_template('homePage.html')
 
 @app.route('/echo', methods=['POST'])
-def hello_world():
-    p = {'street-name': 'Commonwealth', 'city': 'Boston', 'state': 'MA', 'apikey': 'ESjW8PGyvKpXXpVc4VNx5j6alV9tJnT3'}
-    response = requests.get(
-    "https://apis.solarialabs.com/shine/v1/total-home-scores/reports", params=p)
+def user_input():
+    streetnum = request.form['streetnum']
+    streetname = request.form['streetname']
+    city = request.form['city']
+    state = request.form['state']
 
+    p = {'street-number': streetnum, 'street-name': streetname, 'city': city, 'state': state, 'apikey': 'ESjW8PGyvKpXXpVc4VNx5j6alV9tJnT3'}
+    response = requests.get("https://apis.solarialabs.com/shine/v1/total-home-scores/reports", params=p)
     responseObj = json.loads(response._content)
+
     quiet_score = str(responseObj['totalHomeScores']['quiet']['value'])
     safety_score = str(responseObj['totalHomeScores']['safety']['value'])
 
-    output = "The quiet score is " + quiet_score + ", and the safety score is " + safety_score+"."
-    return render_template('homePage.html', address = request.form['address'])
+    return render_template('results.html', quiet_score=quiet_score, safety_score=safety_score)
 
 @app.route('/action_page.php')
 def calculate():
